37279ad824d860aee700160a4fce3f27cb7079d8
Jasveer399 <jasveersingh182002@gmail.com>
2026-05-05 21:40:17 +0530

    feat: enhance report hooks to include admin data validation and store ID in query key

--- src/hooks/useGetAllReports.tsx
+++ src/hooks/useGetAllReports.tsx
@@ -4,8 +4,15 @@ import { useQuery } from "@tanstack/react-query";
 
 export const REPORTS_KEY = "reports";
 
-export const useGetAllReports = () =>
-  useQuery({
-    queryKey: [REPORTS_KEY],
+export const useGetAllReports = () => {
+  const adminDataStr = sessionStorage.getItem("AdminData");
+  if (!adminDataStr) {
+    throw new Error("No admin data found in session storage");
+  }
+  const adminData: { token?: string; store_id?: number } =
+    JSON.parse(adminDataStr);
+  return useQuery({
+    queryKey: [REPORTS_KEY, adminData.store_id],
     queryFn: () => getAllReports(),
   });
+};
--- src/hooks/usegetTodayReport.tsx
+++ src/hooks/usegetTodayReport.tsx
@@ -2,9 +2,15 @@ import { getDashboardReport } from "@/api/dashboard_report";
 import { useQuery } from "@tanstack/react-query";
 
 export const DASHBOARD_REPORT_KEY = "dashboard_report";
-export const useGetDashboardReport = () =>
-  useQuery({
-    queryKey: [DASHBOARD_REPORT_KEY],
+
+export const useGetDashboardReport = () => {
+  const adminDataStr = sessionStorage.getItem("AdminData");
+  const adminData: { token?: string; store_id?: number } = adminDataStr
+    ? JSON.parse(adminDataStr)
+    : {};
+
+  return useQuery({
+    queryKey: [DASHBOARD_REPORT_KEY, adminData.store_id],
     queryFn: () => getDashboardReport(),
-    enabled: true,
   });
+};
