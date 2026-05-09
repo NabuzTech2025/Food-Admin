77e9d5c504fdad646498864a78a4784bf2c6ecfb
Jasveer399 <jasveersingh182002@gmail.com>
2026-05-09 22:17:58 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/v1/admin",
+  base: "/admin",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
