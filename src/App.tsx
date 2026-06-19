520c8b0ea789c89a6852e329922ef7b5836008bb
Jasveer399 <jasveersingh182002@gmail.com>
2026-06-19 18:17:08 +0530

    fix: update router basename and Vite config base path to '/test'

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter basename="/admin">
+        <BrowserRouter basename="/test">
           <NavigateSetter />
           <Routes>
             <Route path="/" element={<Navigate to="/admin-login" replace />} />
