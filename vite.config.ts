ec2449387d99e48453dfd89375dc9227169a6fb1
Jasveer399 <jasveersingh182002@gmail.com>
2026-09-08 15:00:08 +0530

    feat: update deployment configuration for testing environment and adjust base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/admin",
+  base: "/test",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
